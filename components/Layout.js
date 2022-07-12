072c1fe6ea22f0f1b103a9fff1b34413ecd162a5
davaa <m.moo_gii_18@yahoo.com>
2022-07-12 11:45:54 +0800

    'bgndColor'

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Amazona</Typography>
         </Toolbar>
---
+++ utils/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  navbar: {
+    backgroundColor: '#203040',
+    '& a': {
+      color: '#ffffff',
+      marginLeft: 10,
+    },
+  },
+});
+export default useStyles;
