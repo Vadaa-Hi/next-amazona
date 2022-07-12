e03645e25323163cf216aa4089d53f034ba17164
davaa <m.moo_gii_18@yahoo.com>
2022-07-12 12:18:57 +0800

    'refresh'

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from 'react';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  ctx.renderPage = () => {
+    return originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+  };
+  const getInitialProps = await Document.getInitialProps(ctx);
+  return {
+    ...getInitialProps,
+    styles: [
+      ...React.Children.toArray(getInitialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
