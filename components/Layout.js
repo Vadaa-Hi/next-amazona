b3afa4f07f6a5dbdcfe9dae164b943f32f271226
davaa <m.moo_gii_18@yahoo.com>
2022-07-12 13:12:19 +0800

    'RightHeader'

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Link,
+} from '@material-ui/core';
 import useStyles from '../utils/styles';
+import NextLink from 'next/link';
 
 export default function Layout({ children }) {
   const classes = useStyles();
@@ -12,7 +19,20 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Amazona</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
